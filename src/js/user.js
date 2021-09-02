baab8088a3873a2a93a08bb3c1e20b6301be2c3d
Henrik Motzkus <hemotzku@microsoft.com>
2021-09-02 16:29:47 +0200

    javascript for user details

--- src/js/user.js
+++ src/js/user.js
@@ -2,10 +2,17 @@ async function getUserInfo() {
     const response = await fetch('/.auth/me');
     const payload = await response.json();
     const { clientPrincipal } = payload;
+    console.log(getUserInfo());
     return clientPrincipal;
-  }
+}
 
+async function renderUserDetails(){
+    let html = <p>${userDetails}</p>
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
 
-console.log(getUserInfo());
+renderUserDetails();
+
 
 
