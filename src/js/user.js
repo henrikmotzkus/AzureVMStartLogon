4304727393ec4be0ccafad63dec852700cf4bba0
Henrik Motzkus <hemotzku@microsoft.com>
2021-09-02 19:59:00 +0200

    button logic

--- src/js/user.js
+++ src/js/user.js
@@ -18,6 +18,23 @@ async function renderUserDetails(){
     let container2 = document.getElementById('herethevmpanel');
     container2.innerHTML = html2;
 
+    if (user){
+
+        let html3 = `<p><a class="btn btn-primary" href="/.auth/logout">Log out ${user}</a></p>`;
+        let container3 = document.getElementById('heretheloginlogout');
+        container3.innerHTML = html3;
+
+        
+
+    } else {
+
+        let html4 = ` <p><a class="btn btn-primary" href="/.auth/login/aad">Login</a></p>`;
+        let container4 = document.getElementById('heretheloginlogout');
+        container4.innerHTML = html4;
+
+       
+    }
+
 }
 
 
