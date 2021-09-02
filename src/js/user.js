4a1fbcd9a9ade37894178dbb27ab05e0b4964d0c
Henrik Motzkus <hemotzku@microsoft.com>
2021-09-02 19:39:28 +0200

    menu

--- src/js/user.js
+++ src/js/user.js
@@ -13,6 +13,13 @@ async function renderUserDetails(){
     let html = `<p> ${user} </p>`;
     let container = document.getElementById('heretheuser');
     container.innerHTML = html;
+
+    let html2 = `btn btn-primary" href="/StartVM">VM Panel</a></p>`;
+    let container = document.getElementById('herethevmpanel');
+    container.innerHTML = html2;
+
 }
+
+
 
 renderUserDetails();
