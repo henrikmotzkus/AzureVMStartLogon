443dacb7e929585b06cdc23746333b6a81aa2542
Henrik Motzkus <hemotzku@microsoft.com>
2021-09-15 18:19:20 +0200

    a

--- src2/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ src2/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace VMStarter.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 1 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 2 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 3 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 4 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 5 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 6 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 7 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 8 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 9 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 10 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using VMStarter;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 11 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using VMStarter.Shared;
 
 #line default
--- src2/obj/Debug/net5.0/RazorDeclaration/Shared/LoginDisplay.razor.g.cs
+++ src2/obj/Debug/net5.0/RazorDeclaration/Shared/LoginDisplay.razor.g.cs
@@ -13,84 +13,84 @@ namespace VMStarter.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 1 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 2 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 4 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 5 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 6 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 7 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 8 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 9 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 10 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using VMStarter;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\_Imports.razor"
+#line 11 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\_Imports.razor"
 using VMStarter.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 1 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\Shared\LoginDisplay.razor"
+#line 1 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\Shared\LoginDisplay.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\Shared\LoginDisplay.razor"
+#line 2 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\Shared\LoginDisplay.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 #line default
@@ -104,7 +104,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 17 "C:\Users\hemotzku\source\repos\VMStarter\VMStarter\Shared\LoginDisplay.razor"
+#line 17 "C:\Users\hemotzku\Github\AzureVMStartLogon\src2\Shared\LoginDisplay.razor"
       
     private async Task BeginLogout(MouseEventArgs args)
     {
