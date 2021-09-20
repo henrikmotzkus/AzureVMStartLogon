34d9211557a55510aacf74371e48a3c11c09088f
Henrik Motzkus <hemotzku@microsoft.com>
2021-09-20 08:39:54 +0200

    array

--- Function/GetVM/run.ps1
+++ Function/GetVM/run.ps1
@@ -8,10 +8,15 @@ Write-Host "GetVM triggered"
 
 # Get the VMs from a file
 
-$body = get-content -Path "./GetVM/vms.json"
+get-content ./GetVM/vms.json
+
+$body = ConvertTo-Json -InputObject $vms
+
+$bodies = @($body)
+
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body = $body
+    Body = $bodies
 })
